bc0b49e901eda1e6ab8261b452576432a1035729
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2020-03-01 17:02:53 +0100

    Moved `make_action_class` to separate project.

--- usernamegen.py
+++ usernamegen.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
-from argparse import ArgumentParser, FileType, Action, Namespace
+from argparse import ArgumentParser, FileType
 from itertools import product
 from sys import stdout
-from io import TextIOWrapper
-from typing import List, Set, Union, Optional, Type, Iterable
+from typing import Set, Iterable
 from importlib import resources as importlib_resources
+
+from pyutils.argparse import make_action_class
 
 import resources
 
@@ -59,50 +60,6 @@ def usernamegen(
             user_names.add(username)
 
     return user_names
-
-
-# TODO: Move to some other project in future?
-def make_action_class(collection_name: str) -> Type[Action]:
-    """
-    Create an `argparse.Action` class that stores entries in a certain attribute in the `argparse` namespace.
-
-    Supports entries in the form of strings and `argparse.FileType` file wrappers.
-
-    :param collection_name: The name of the attribute in `argparse` namespace that is to store the entries.
-    :return: A dynamically created custom `argparse.Action` class.
-    """
-
-    # NOTE: The name will not be unique.
-    action_class = type(f'{collection_name}Action', (Action,), dict())
-
-    def __call__(
-        self,
-        _: ArgumentParser,
-        namespace: Namespace,
-        values: Union[List[str], List[TextIOWrapper]],
-        __: Optional[str] = None
-    ) -> None:
-
-        collected_set: Set[str] = getattr(namespace, collection_name, set())
-
-        if isinstance(self.type, FileType):
-            value_files: List[TextIOWrapper] = values
-            values_to_be_added = (
-                value.strip()
-                for value_file in value_files
-                for value in value_file.read().splitlines()
-            )
-        else:
-            values_to_be_added = values
-
-        collected_set.update(values_to_be_added)
-
-        setattr(namespace, self.dest, values)
-        setattr(namespace, collection_name, collected_set)
-
-    setattr(action_class, '__call__', __call__)
-
-    return action_class
 
 
 def get_parser(
